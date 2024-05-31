d65a3842266ce95d816ed9bbd2c5dea690248e24
Fran <frnga1987@gmail.com>
2024-05-31 13:02:26 +0200

    Commit 8: Eliminar pedido

--- src/main/java/com/spring/productsshop/controller/ClientController.java
+++ src/main/java/com/spring/productsshop/controller/ClientController.java
@@ -37,7 +37,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Client", description = "Client controller with CRUD Operations")
 public class ClientController {
 
-    private final ClientService clientService;
+    private final ClientService clientService; 
 
     @Autowired
     public ClientController(ClientService clientService) {
@@ -48,6 +48,7 @@ public class ClientController {
 
     // -------------- Petición todos los clientes --------------//
 
+    // Petición GET para obtener todos los clientes
     @Operation(summary = "Get all clients", description = "Get a list of all clients")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Clients found, retrieved clients", content = {
@@ -58,15 +59,19 @@ public class ClientController {
     })
     @GetMapping("/clients")
     public ResponseEntity<List<ClientDTO>> getAllClients() {
+        // Llama al servicio para obtener todos los clientes
         List<Client> clients = clientService.getAllClients();
+        // Si no se encuentran clientes, lanza una excepción de recurso no encontrado
         if (clients.isEmpty()) {
             throw new ResourceNotFoundException("No clients found");
         }
+        // Convierte la lista de clientes en una lista de DTO de clientes y la devuelve en una ResponseEntity
         return ResponseEntity.ok(ClientConvertTo.convertToDTOList(clients));
     }
 
     // -------------- Petición cliente por ID --------------//
 
+    // Petición GET para obtener un cliente por su ID
     @Operation(summary = "Get client by ID", description = "Get a client by its ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Client found", content = {
@@ -75,15 +80,18 @@ public class ClientController {
     })
     @GetMapping("/clients/{id}")
     public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id) {
+        // Llama al servicio para obtener un cliente por su ID
         Optional<Client> clientOptional = clientService.getClientById(id);
+        // Si no se encuentra el cliente, lanza una excepción de recurso no encontrado
         Client client = clientOptional
                 .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + id));
+        // Convierte el cliente en un DTO de cliente y lo devuelve en una ResponseEntity
         return ResponseEntity.ok(ClientConvertTo.convertToDTO(client));
     }
 
     // -------------- Métodos peticiones Insertar --------------//
 
-    // Petición para insertar nuevo cliente
+    // Petición POST para crear un nuevo cliente
     @Operation(summary = "Create a new client", description = "Create a new client")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Client created"),
@@ -91,11 +99,15 @@ public class ClientController {
     })
     @PostMapping("/clients")
     public ResponseEntity<ClientDTO> createClient(@RequestBody ClientDTO clientDTO) {
+        // Si los datos del cliente son inválidos, lanza una excepción de validación
         if (clientDTO == null) {
             throw new ValidationException("Invalid client data provided");
         }
+        // Convierte el DTO de cliente en una entidad de cliente
         Client client = ClientConvertTo.convertToEntity(clientDTO);
+        // Crea un nuevo cliente en la base de datos
         Client savedClient = clientService.createClient(client);
+        // Convierte el cliente guardado en un DTO de cliente y lo devuelve en una ResponseEntity con el código de estado 201 (CREATED)
         return ResponseEntity.status(HttpStatus.CREATED).body(ClientConvertTo.convertToDTO(savedClient));
     }
 
@@ -103,7 +115,7 @@ public class ClientController {
 
     // -------------- Petición modificación total cliente --------------//
 
-    // Petición para modificar cliente
+    // Petición PUT para actualizar un cliente por su ID
     @Operation(summary = "Update client by ID", description = "Update an existing client by its ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Client updated"),
@@ -111,17 +123,17 @@ public class ClientController {
     })
     @PutMapping("/clients/{id}")
     public ResponseEntity<ClientDTO> updateClient(@PathVariable Long id, @RequestBody ClientDTO updatedClientDTO) {
-        // Convierte el DTO del cliente en una entidad de cliente
+        // Convierte el DTO de cliente actualizado en una entidad de cliente
         Client updatedClient = ClientConvertTo.convertToEntity(updatedClientDTO);
         // Actualiza el cliente en la base de datos
         Client savedClient = clientService.updateClient(id, updatedClient);
-        // Devuelve el cliente actualizado en formato DTO
+        // Devuelve el cliente actualizado en formato DTO en una ResponseEntity
         return ResponseEntity.ok(ClientConvertTo.convertToDTO(savedClient));
     }
 
     // -------------- Petición modificación de atributo cliente --------------//
 
-    // Método para actualizar solo el nombre de un cliente
+    // Método PATCH para actualizar solo el nombre de un cliente
     @Operation(summary = "Update client name by ID", description = "Update the name of an existing client by its ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Client name updated"),
@@ -129,12 +141,15 @@ public class ClientController {
     })
     @PatchMapping("/clients/{id}/updateName")
     public ResponseEntity<ClientDTO> updateClientName(@PathVariable Long id, @RequestBody String newName) {
+        // Actualiza el nombre del cliente en la base de datos
         Client savedClient = clientService.updateClientName(id, newName);
+        // Devuelve el cliente actualizado en formato DTO en una ResponseEntity
         return ResponseEntity.ok(ClientConvertTo.convertToDTO(savedClient));
     }
 
     // -------------- Métodos peticiones Borrar --------------//
 
+    // Petición DELETE para eliminar un cliente por su ID
     @Operation(summary = "Delete client by ID", description = "Delete a client by its ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Client deleted"),
@@ -142,7 +157,9 @@ public class ClientController {
     })
     @DeleteMapping("/clients/{id}")
     public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        // Elimina el cliente de la base de datos
         clientService.deleteClient(id);
+        // Devuelve una ResponseEntity sin contenido con el código de estado 204 (NO CONTENT)
         return ResponseEntity.noContent().build();
     }
 }
@@ -151,159 +168,8 @@ public class ClientController {
 
 
 
-/*
- * @RestController
- * 
- * @Tag(name = "Client", description = "Client controller with CRUD Operations")
- * public class ClientController {
- * 
- * private final ClientRepository clientRepository;
- * 
- * @Autowired public ClientController(ClientRepository clientRepository) {
- * this.clientRepository = clientRepository; }
- * 
- * // -------------- Métodos peticiones Consultar --------------//
- * 
- * // -------------- Petición todos los clientes --------------//
- * 
- * @Operation(summary = "Get all clients", description =
- * "Get a list of all clients")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "200", description =
- * "Clients found, retrieved clients", content = {
- * 
- * @Content(mediaType = "application/json", array = @ArraySchema(schema
- * = @Schema(implementation = Client.class))) }),
- * 
- * @ApiResponse(responseCode = "404", description = "Clients not found", content
- * = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @GetMapping("/clients") public ResponseEntity<List<ClientDTO>>
- * getAllClients() { List<Client> clients = clientRepository.findAll(); if
- * (clients.isEmpty()) { throw new
- * ResourceNotFoundException("No clients found"); } return
- * ResponseEntity.ok(ClientConvertTo.convertToDTOList(clients)); }
- * 
- * // -------------- Petición cliente por ID --------------//
- * 
- * @Operation(summary = "Get client by ID", description =
- * "Get a client by its ID")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "200", description = "Client found", content = {
- * 
- * @Content(mediaType = "application/json")}),
- * 
- * @ApiResponse(responseCode = "404", description =
- * "Client not found with id: ...", content = @Content(schema
- * = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @GetMapping("/clients/{id}") public ResponseEntity<ClientDTO>
- * getClientById(@PathVariable Long id) { Optional<Client> clientOptional =
- * clientRepository.findById(id); Client client = clientOptional .orElseThrow(()
- * -> new ResourceNotFoundException("Client not found with id: " + id)); return
- * ResponseEntity.ok(ClientConvertTo.convertToDTO(client)); }
- * 
- * // -------------- Métodos peticiones Insertar --------------//
- * 
- * // Petición para insertar nuevo cliente
- * 
- * @Operation(summary = "Create a new client", description =
- * "Create a new client")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "201", description = "Client created"),
- * 
- * @ApiResponse(responseCode = "400", description =
- * "Invalid client data provided", content = @Content(schema
- * = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @PostMapping("/clients") public ResponseEntity<ClientDTO>
- * createClient(@RequestBody ClientDTO clientDTO) { if (clientDTO == null) {
- * throw new ValidationException("Invalid client data provided"); } Client
- * client = ClientConvertTo.convertToEntity(clientDTO); Client savedClient =
- * clientRepository.save(client); return
- * ResponseEntity.status(HttpStatus.CREATED).body(ClientConvertTo.convertToDTO(
- * savedClient)); }
- * 
- * // -------------- Métodos peticiones Modificar --------------//
- * 
- * // -------------- Petición modificación total cliente --------------//
- * 
- * // Petición para modificar cliente
- * 
- * @Operation(summary = "Update client by ID", description =
- * "Update an existing client by its ID")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "200", description = "Client updated"),
- * 
- * @ApiResponse(responseCode = "404", description =
- * "Client not found with id: ...", content = @Content(schema
- * = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @PutMapping("/clients/{id}") public ResponseEntity<ClientDTO>
- * updateClient(@PathVariable Long id, @RequestBody ClientDTO updatedClientDTO)
- * { // Busca cliente por id para guardarlo en un objeto cliente
- * Optional<Client> clientOptional = clientRepository.findById(id); Client
- * client = clientOptional .orElseThrow(() -> new
- * ResourceNotFoundException("Client not found with id: " + id)); // Guarda los
- * atributos que se pasan en el cuerpo de la petición
- * client.setName(updatedClientDTO.getName());
- * client.setAddress(updatedClientDTO.getAddress());
- * client.setAge(updatedClientDTO.getAge()); // Guarda el cliente modificado y y
- * convierte en DTO para enviar los datos a la // respuesta Client savedClient =
- * clientRepository.save(client); return
- * ResponseEntity.ok(ClientConvertTo.convertToDTO(savedClient)); }
- * 
- * // -------------- Petición modificación de atributo cliente --------------//
- * 
- * // Método para actualizar solo el nombre de un cliente
- * 
- * @Operation(summary = "Update client name by ID", description =
- * "Update the name of an existing client by its ID")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "200", description = "Client name updated"),
- * 
- * @ApiResponse(responseCode = "404", description =
- * "Client not found with id: ...", content = @Content(schema
- * = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @PatchMapping("/clients/{id}/updateName") public ResponseEntity<ClientDTO>
- * updateClientName(@PathVariable Long id, @RequestBody String newName) {
- * Optional<Client> clientOptional = clientRepository.findById(id); Client
- * client = clientOptional .orElseThrow(() -> new
- * ResourceNotFoundException("Client not found with id: " + id)); // Actualizar
- * solo el nombre del cliente client.setName(newName); // Guardar el cliente
- * actualizado en la base de datos Client savedClient =
- * clientRepository.save(client); // Retornar el cliente actualizado en formato
- * DTO return ResponseEntity.ok(ClientConvertTo.convertToDTO(savedClient)); }
- * 
- * // -------------- Métodos peticiones Borrar --------------//
- * 
- * @Operation(summary = "Delete client by ID", description =
- * "Delete a client by its ID")
- * 
- * @ApiResponses(value = {
- * 
- * @ApiResponse(responseCode = "204", description = "Client deleted"),
- * 
- * @ApiResponse(responseCode = "404", description =
- * "Client not found with id: ...", content = @Content(schema
- * = @Schema(implementation = ErrorDetails.class))) })
- * 
- * @DeleteMapping("/clients/{id}") public ResponseEntity<Void>
- * deleteClient(@PathVariable Long id) { if (clientRepository.existsById(id)) {
- * clientRepository.deleteById(id); return ResponseEntity.noContent().build(); }
- * else { throw new ResourceNotFoundException("Client not found with id: " +
- * id); } } }
- */
+
+
+
 
 
--- src/main/java/com/spring/productsshop/controller/OrderController.java
+++ src/main/java/com/spring/productsshop/controller/OrderController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,29 +52,27 @@ public class OrderController {
 		this.orderRepository = orderRepository;
 		this.orderItemRepository = orderItemRepository;
 		this.productRepository = productRepository;
-		this.clientRepository=clientRepository;
+		this.clientRepository = clientRepository;
 	}
-	
-	
-	// ---------------------Método para consultar pedido por cliente---------------------//
-	@Operation(summary = "Get orders by client", description = "Get orders by client")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Orders found, retrieved orders", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = OrderListDTO.class)) }),
-            @ApiResponse(responseCode = "404", description = "Orders not found", content = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
-    @GetMapping("/orders")
-    public ResponseEntity<List<OrderDTO>> getOrdersByClient(@RequestParam Long clientId) {
-        List<Order> orders = orderRepository.findByClientId(clientId);
-        if (orders.isEmpty()) {
-            throw new ResourceNotFoundException("No orders found for client with ID: " + clientId);
-        }
-        List<OrderDTO> orderDTOs = orders.stream().map(OrderConvertTo::convertToDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(orderDTOs);
-    }
 
-	
-	// ---------------------Método para consultar pedido por id---------------------//
+	// --------------------- Método para consultar pedidos por cliente ---------------------//
+	@Operation(summary = "Get orders by client", description = "Get orders by client")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Orders found, retrieved orders", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = OrderListDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "Orders not found", content = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
+	@GetMapping("/orders")
+	public ResponseEntity<List<OrderDTO>> getOrdersByClient(@RequestParam Long clientId) {
+		List<Order> orders = orderRepository.findByClientId(clientId); // Consultar pedidos por ID de cliente
+		if (orders.isEmpty()) {
+			throw new ResourceNotFoundException("No orders found for client with ID: " + clientId); 
+		}
+		List<OrderDTO> orderDTOs = orders.stream().map(OrderConvertTo::convertToDTO) // Convertir entidades de pedido a DTO
+				.collect(Collectors.toList());
+		return ResponseEntity.ok(orderDTOs); // Devolver lista de pedidos
+	}
+
+	// --------------------- Método para consultar pedido por ID ---------------------//
 	@Operation(summary = "Get orders by id", description = "Get orders by id")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Orders found, retrieved orders", content = {
@@ -81,13 +80,12 @@ public class OrderController {
 			@ApiResponse(responseCode = "404", description = "Orders not found", content = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
 	@GetMapping("/orders/{id}")
 	public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long id) {
-		Order order = orderRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Order not found with id: " + id));
-		return ResponseEntity.ok(OrderConvertTo.convertToDTO(order));
+		Order order = orderRepository.findById(id) // Consultar pedido por ID
+				.orElseThrow(() -> new ResourceNotFoundException("Order not found with id: " + id)); 
+		return ResponseEntity.ok(OrderConvertTo.convertToDTO(order)); // Devolver DTO del pedido encontrado
 	}
-	
-	
-	// ---------------------Método para crear un nuevo pedido---------------------//
+
+	//--------------------- Método para crear un nuevo pedido ---------------------//
 	@Operation(summary = "Create a new order", description = "Create a new order")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "201", description = "Order created", content = {
@@ -95,41 +93,53 @@ public class OrderController {
 			@ApiResponse(responseCode = "400", description = "Invalid data provided", content = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
 	@PostMapping("/orders")
 	public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
-	    try {
-	        // Buscar el cliente por su ID
-	        Client client = clientRepository.findById(orderDTO.getClientId())
-	            .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + orderDTO.getClientId()));
+		try {
+			// Buscar el cliente por su ID
+			Client client = clientRepository.findById(orderDTO.getClientId()).orElseThrow(
+					() -> new ResourceNotFoundException("Client not found with id: " + orderDTO.getClientId()));
 
-	        // Buscar los productos por sus IDs
-	        List<Product> products = productRepository.findAllById(
-	            orderDTO.getItems().stream().map(OrderItemDTO::getProductId).collect(Collectors.toList())
-	        );
+			// Buscar los productos por sus IDs
+			List<Product> products = productRepository.findAllById(
+					orderDTO.getItems().stream().map(OrderItemDTO::getProductId).collect(Collectors.toList()));
 
-	        // Convertir la orden DTO en una entidad Order
-	        Order order = new Order();
-	        order.setClient(client);  // Vincular el cliente al pedido
-	        order.setItems(new ArrayList<>());
+			// Convertir la orden DTO en una entidad Order
+			Order order = new Order();
+			order.setClient(client); // Vincular el cliente al pedido
+			order.setItems(new ArrayList<>());
 
-	        // Guardar el pedido
-	        Order savedOrder = orderRepository.save(order);
-	        List<OrderItem> orderItems = orderDTO.getItems().stream().map(dto -> {
-	            Product product = products.stream().filter(p -> p.getId().equals(dto.getProductId())).findFirst()
-	                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + dto.getProductId()));
-	            return new OrderItem(null, product, dto.getQuantity(), dto.getPrice(), savedOrder);
-	        }).collect(Collectors.toList());
+			// Guardar el pedido
+			Order savedOrder = orderRepository.save(order);
+			List<OrderItem> orderItems = orderDTO.getItems().stream().map(dto -> {
+				Product product = products.stream().filter(p -> p.getId().equals(dto.getProductId())).findFirst()
+						.orElseThrow(() -> new ResourceNotFoundException(
+								"Product not found with id: " + dto.getProductId()));
+				return new OrderItem(null, product, dto.getQuantity(), dto.getPrice(), savedOrder);
+			}).collect(Collectors.toList());
 
-	        orderItemRepository.saveAll(orderItems);
+			orderItemRepository.saveAll(orderItems);
 
-	        // Calcular el total del pedido
-	        double total = orderItems.stream().mapToDouble(item -> item.getPrice() * item.getQuantity()).sum();
-	        savedOrder.setTotal(total);
-	        orderRepository.save(savedOrder);
+			// Calcular el total del pedido
+			double total = orderItems.stream().mapToDouble(item -> item.getPrice() * item.getQuantity()).sum();
+			savedOrder.setTotal(total);
+			orderRepository.save(savedOrder);
 
-	        // Devolver el pedido guardado
-	        return ResponseEntity.status(HttpStatus.CREATED).body(OrderConvertTo.convertToDTO(savedOrder));
-	    } catch (Exception ex) {
-	        // Manejar cualquier error y devolver un mensaje de error apropiado
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-	    }
+			// Devolver el pedido guardado
+			return ResponseEntity.status(HttpStatus.CREATED).body(OrderConvertTo.convertToDTO(savedOrder));
+		} catch (Exception ex) {
+			// Manejar cualquier error y devolver un mensaje de error apropiado
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	//--------------------- Método para eliminar un pedido por su ID ---------------------//
+	@Operation(summary = "Delete order by ID", description = "Delete order by ID")
+	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Order deleted"),
+			@ApiResponse(responseCode = "404", description = "Order not found", content = @Content(schema = @Schema(implementation = ErrorDetails.class))) })
+	@DeleteMapping("/orders/{id}")
+	public ResponseEntity<Void> deleteOrderById(@PathVariable Long id) {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Order not found with id: " + id));
+		orderRepository.delete(order);
+		return ResponseEntity.noContent().build();
 	}
 }
